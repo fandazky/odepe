240e28e9dc167c324e1db5b1367230a3a4aad517
fandazky <fandy12@mhs.if.its.ac.id>
2015-07-02 09:05:21 +0700

    update database

--- related dokumen/db_odepe v1.1.sql
+++ related dokumen/db_odepe v1.1.sql
@@ -3,9 +3,9 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 30, 2015 at 09:24 AM
+-- Generation Time: Jul 02, 2015 at 04:02 AM
 -- Server version: 5.6.21
--- PHP Version: 5.6.3
+-- PHP Version: 5.5.19
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -162,18 +162,20 @@ CREATE TABLE IF NOT EXISTS `registration_request` (
   `password` varchar(32) NOT NULL,
   `first_name` varchar(30) NOT NULL,
   `last_name` varchar(30) DEFAULT NULL,
-  `address` varchar(50) DEFAULT NULL
+  `address` varchar(50) DEFAULT NULL,
+  `level` int(1) NOT NULL,
+  `status` int(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `registration_request`
 --
 
-INSERT INTO `registration_request` (`username`, `password`, `first_name`, `last_name`, `address`) VALUES
-('alibaba', 'asdfg', 'bismillah', 'alhamdulillah', 'Oroginak'),
-('cek@gmail.com', 'ccekcek', 'first', 'last', 'Bumi Marina Emas'),
-('cekcek@gmail.com', 'haihai', 'firstname', 'lastname', 'California, US'),
-('fafa@gmail.com', '123', 'pertamax', 'keduax', 'San Fransisco, US');
+INSERT INTO `registration_request` (`username`, `password`, `first_name`, `last_name`, `address`, `level`, `status`) VALUES
+('alibaba', 'asdfg', 'bismillah', 'alhamdulillah', 'Oroginak', 2, 1),
+('cek@gmail.com', 'ccekcek', 'first', 'last', 'Bumi Marina Emas', 2, 1),
+('cekcek@gmail.com', 'haihai', 'firstname', 'lastname', 'California, US', 2, 1),
+('fafa@gmail.com', '123', 'pertamax', 'keduax', 'San Fransisco, US', 2, 1);
 
 -- --------------------------------------------------------
 
